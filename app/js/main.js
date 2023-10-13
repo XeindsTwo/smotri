0377d573a49b9d0a1a8490b3625f0b3151b230ba
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2023-10-14 00:14:39 +0300

    Доделана секция механизмы. Дальше - адаптив хедера (начато)

--- app/js/main.js
+++ app/js/main.js
@@ -76,7 +76,7 @@ buttons.forEach(button => {
     });
 });
 
-const btnFooter = document.querySelector('.footer__menu-btn');
+const btnFooter = document.querySelector('.menu-btn--footer');
 const menuFooter = document.querySelector('.footer__nav-right--mobile');
 
 btnFooter.addEventListener('click', () => {
@@ -94,47 +94,24 @@ btnFooter.addEventListener('click', () => {
 });
 
 const mechanismItems = document.getElementById("mechanismItems");
-let animationActive = false;
+const items = document.querySelectorAll(".mechanism__item");
+items.forEach((item) => {
+    const clonedItem = item.cloneNode(true);
+    mechanismItems.appendChild(clonedItem);
+});
 
-function initAnimation() {
-    const items = document.querySelectorAll(".mechanism__item");
-    items.forEach((item) => {
-        const clonedItem = item.cloneNode(true);
-        mechanismItems.appendChild(clonedItem);
-    });
+let scrollAmount = 0;
 
-    let scrollAmount = 0;
-
-    function scrollItems() {
-        scrollAmount += 1;
-        mechanismItems.style.transform = `translateX(-${scrollAmount}px)`;
-        if (scrollAmount >= mechanismItems.offsetWidth / 2) {
-            scrollAmount = 0;
-            mechanismItems.style.transform = "translateX(0)";
-        }
-    }
-
-    animationActive = true;
-    setInterval(scrollItems, 10);
-}
-
-function stopAnimation() {
-    while (mechanismItems.firstChild) {
-        mechanismItems.removeChild(mechanismItems.firstChild);
-    }
-    animationActive = false;
-}
-
-function handleResize() {
-    if (window.innerWidth < 1260 && !animationActive) {
-        initAnimation();
-    } else if (window.innerWidth >= 1260 && animationActive) {
-        stopAnimation();
+function scrollItems() {
+    scrollAmount += 1;
+    mechanismItems.style.transform = `translateX(-${scrollAmount}px)`;
+    if (scrollAmount >= mechanismItems.offsetWidth / 2) {
+        scrollAmount = 0;
+        mechanismItems.style.transform = "translateX(0)";
     }
 }
 
-handleResize();
-window.addEventListener("resize", handleResize);
+setInterval(scrollItems, 10);
 
 /*
 const tariffSection = document.getElementById('tariff');
